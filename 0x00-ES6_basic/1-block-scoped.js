e86e19ea860c268555d5192f1930428bd1e13891
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-01-04 00:49:00 +0100

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
-  let task2 = true;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true; // Create a variable task
-    task2 = false; // Create a variable task2
+    task = !task; // Modify a variable task
+    task = !task2; // Modify a variable task2
   }
 
   return [task, task2];
