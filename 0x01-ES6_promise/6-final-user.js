655726f2634c651587da3646d3bc4ce3aa4689be
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-01-03 21:49:11 +0100

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(firstName)])
+    .then((i) => (
+      i.map((j) => ({
+        status: j.status,
+        value: j.status === 'full' ? j.value : String(j.reason),
+      }))
+    ));
+}
