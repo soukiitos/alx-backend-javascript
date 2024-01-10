c01c228ffd8c50ce2ac6c3b26f91b161364cb67e
Soukaina Lachheb <soukainalachheb@gmail.com>
2024-01-10 01:02:25 +0100

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+export const weakMap = new WeakMap();
+
+export default function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    const wkM = weakMap.get(endpoint);
+    if (wkM >= 4) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, (wkM + 1));
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}
