dab5cc6c0fe09845d287e88030868cda102fd648
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-27 23:18:11 +0100

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -9,19 +9,19 @@ function countStudents(path) {
             }
             const response = [];
             let msg;
-            
+
             const content = data.toString().split('\n');
-            
+
             let students = content.filter((item) => item);
-            
-            students = students.map((item) => item.split(','));
-            
+
+            students = students.map((item) => item.split(', '));
+
             const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
             msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
             console.log(msg);
-            
+
             response.push(msg);
-            
+
             const fields = {};
             for (const i in students) {
                 if (i !== 0) {
@@ -32,14 +32,14 @@ function countStudents(path) {
             }
             
             delete fields.field;
-            
+
             for (const key of Object.keys(fields)) {
                 msg = `Number of students in ${key}: ${
                     fields[key].length
                 }. List: ${fields[key].join(', ')}`;
                 
                 console.log(msg);
-                
+
                 response.push(msg);
             }
             resolve(response);
