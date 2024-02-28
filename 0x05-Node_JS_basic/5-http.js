c6eef97a9664d993c689894942bd0b5c9e7594da
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-28 01:16:18 +0100

    Create a more complex HTTP server using Node s HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,10 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+
 const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
 const DATABASE = args[0];
+
 const hostname = '127.0.0.1';
 const port = 1245;
 
