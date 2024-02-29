0b8c53644d931d2aebea8b578eb33286314b089f
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-29 21:44:52 +0100

    Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+var { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('Spy', () => {
+  it('Should ensure that the math is the same.', () => {
+    const spyUtils = sinon.spy(Utils, 'calculateNumber');
+    const consSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spyUtils.calledOncewithExaxtly('SUM', 100, 20)).to.be.true;
+    expect(consSpy.calledOncewithExaxtly('The total is: 120')).to.be.true;
+
+    spyUtils.restore();
+    consSpy.restore();
+  });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,20 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+  const firstNum = Math.round(a);
+  const secondNum = Math.round(b);
+
+  if (type === 'SUBTRACT') {
+    return firstNum - secondNum;
+  }
+
+  if (type === 'DIVIDE') {
+    if (secondNum === 0) {
+      return 'Error';
+    }
+    return firstNum / secondNum;
+  }
+
+  return firstNum + secondNum;
+}
+
+module.exports = Utils;
