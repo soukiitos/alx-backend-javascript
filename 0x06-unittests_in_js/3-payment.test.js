de18077170adeb23ec86c452c12de6bff99bfec3
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-29 22:07:12 +0100

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -11,8 +11,8 @@ describe('Spy', () => {
 
     sendPaymentRequestToApi(100, 20);
 
-    expect(spyUtils.calledOnceWithExaxtly('SUM', 100, 20)).to.be.true;
-    expect(consSpy.calledOnceWithExaxtly('The total is: 120')).to.be.true;
+    expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
 
     spyUtils.restore();
     consSpy.restore();
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,20 +1,20 @@
 const Utils = {
   calculateNumber(type, a, b) {
-  const firstNum = Math.round(a);
-  const secondNum = Math.round(b);
+    const firstNum = Math.round(a);
+    const secondNum = Math.round(b);
 
-  if (type === 'SUBTRACT') {
-    return firstNum - secondNum;
-  }
-
-  if (type === 'DIVIDE') {
-    if (secondNum === 0) {
-      return 'Error';
+    if (type === 'SUBTRACT') {
+      return firstNum - secondNum;
     }
-    return firstNum / secondNum;
-  }
 
-  return firstNum + secondNum;
+    if (type === 'DIVIDE') {
+      if (secondNum === 0) {
+        return 'Error';
+      }
+      return firstNum / secondNum;
+    }
+
+    return firstNum + secondNum;
   }
 };
 
