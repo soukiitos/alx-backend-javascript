02ccaa979541ce34585b458d34a81fd352076eb0
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-29 22:15:23 +0100

    Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('Stub', () => {
+  it('Should log the correct msg and stub the calculateNumber', () => {
+    const stubUtils = sinon.stub(Utils, 'calculateNumber').return(10);
+    const consSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(stubUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+    stubUtils.restore();
+    consSpy.restore();
+  });
+});
